7ff27463c5202f8efb683375d9f478e5a2d273d7
damianbalinski <damian.balinski.99@gmail.com>
2021-07-28 20:51:59 +0200

    cleanup

--- include/sorting/insertion_sort.hpp
+++ include/sorting/insertion_sort.hpp
@@ -7,5 +7,5 @@ public:
 	void operator()(int arr[], const size_t n, const comparator& comp) const override { sort(arr, n, comp); }
 
 private:
-	static void sort(int arr[], const size_t n, const comparator& comp);
+	static void sort(int arr[], size_t n, const comparator& comp);
 };
