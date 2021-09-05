64edcbbdb111e8cc60b3d823dc385a5fe05c4c2d
damianbalinski <damian.balinski.99@gmail.com>
2021-09-05 12:06:43 +0200

    sort - heap sort

---
+++ include/sorting/heap_sort.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include "sorting.hpp"
+#include "heap.hpp"
+#include "binary_max_heap.hpp"
+
+class heap_sort : public sorting
+{
+public:
+	void operator()(int arr[], const size_t n, const comparator& comp) const override { sort(arr, n, comp, binary_max_heap()); }
+
+private:
+	static void sort(int arr[], size_t n, const comparator& comp, const heap& heap);
+};
