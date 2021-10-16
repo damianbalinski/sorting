cad44075bc47102c5881c34642311b1f45d6fcb4
damianbalinski <damianbalinski99@gmail.com>
2021-10-16 17:36:00 +0200

    inject swapper into operation

--- include/sorting/operation.hpp
+++ include/sorting/operation.hpp
@@ -1,17 +1,20 @@
 #pragma once
 #include "comparator.hpp"
+#include "swapper.hpp"
 
 class operation
 {
 public:
-	operation(const comparator& comp): comp(comp) {}
-	bool eq(int a, int b) const { return comp.eq(a, b); }
-	bool ne(int a, int b) const { return comp.ne(a, b); }
-	bool gt(int a, int b) const { return comp.gt(a, b); }
-	bool ge(int a, int b) const { return comp.ge(a, b); }
-	bool lt(int a, int b) const { return comp.lt(a, b); }
-	bool le(int a, int b) const { return comp.le(a, b); }
+	operation(const comparator& comp, const swapper& swapper): comp(comp), swapper(swapper) {}
+	bool eq(const int a, const int b) const { return comp.eq(a, b); }
+	bool ne(const int a, const int b) const { return comp.ne(a, b); }
+	bool gt(const int a, const int b) const { return comp.gt(a, b); }
+	bool ge(const int a, const int b) const { return comp.ge(a, b); }
+	bool lt(const int a, const int b) const { return comp.lt(a, b); }
+	bool le(const int a, const int b) const { return comp.le(a, b); }
+	void swap(int* const a, int* const b)   { swapper.swap(a, b);   }
 
 private:
 	const comparator& comp;
+	const swapper& swapper;
 };
--- include/sorting/swapper.hpp
+++ include/sorting/swapper.hpp
@@ -1,8 +1,18 @@
 #pragma once
-#include "swap.hpp"
 
 class swapper
 {
 public:
-	void operator()(int* const a, int* const b) const { swap(a, b); }
+	virtual void swap(int* a, int* b) const = 0;
+};
+
+class simple_swapper : public swapper
+{
+public:
+	void swap(int* const a, int* const b) const override
+	{
+		const int temp = *a;
+		*a = *b;
+		*b = temp;
+	}
 };
