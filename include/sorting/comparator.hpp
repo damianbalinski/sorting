634766a10f31869edeabc9ab70fd47747e9c80ab
damianbalinski <damianbalinski99@gmail.com>
2021-10-16 12:42:04 +0200

    add counting comparators

--- include/sorting/comparator.hpp
+++ include/sorting/comparator.hpp
@@ -32,3 +32,33 @@ public:
 	bool lt(const int a, const int b) const override { return a >  b; }
 	bool le(const int a, const int b) const override { return a >= b; }
 };
+
+class counting_asc_comparator : public comparator
+{
+public:
+	counting_asc_comparator(size_t* const counter): counter(counter) {}
+	bool eq(const int a, const int b) const override { (*counter)++; return a == b; }
+	bool ne(const int a, const int b) const override { (*counter)++; return a != b; }
+	bool gt(const int a, const int b) const override { (*counter)++; return a >  b; }
+	bool ge(const int a, const int b) const override { (*counter)++; return a >= b; }
+	bool lt(const int a, const int b) const override { (*counter)++; return a <  b; }
+	bool le(const int a, const int b) const override { (*counter)++; return a <= b; }
+
+private:
+	size_t* const counter;
+};
+
+class counting_desc_comparator : public comparator
+{
+public:
+	counting_desc_comparator(size_t* const counter): counter(counter) {}
+	bool eq(const int a, const int b) const override { (*counter)++; return a == b; }
+	bool ne(const int a, const int b) const override { (*counter)++; return a != b; }
+	bool gt(const int a, const int b) const override { (*counter)++; return a <  b; }
+	bool ge(const int a, const int b) const override { (*counter)++; return a <= b; }
+	bool lt(const int a, const int b) const override { (*counter)++; return a >  b; }
+	bool le(const int a, const int b) const override { (*counter)++; return a >= b; }
+
+private:
+	size_t* const counter;
+};
