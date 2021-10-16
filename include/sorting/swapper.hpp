41dff578bc013717b43260584486d39510370eec
damian.balinski <damian.balinski@infinite.pl>
2021-10-16 20:56:41 +0200

    counting swapper

--- include/sorting/swapper.hpp
+++ include/sorting/swapper.hpp
@@ -1,5 +1,12 @@
 #pragma once
 
+inline void __swap(int* const a, int* const b)
+{
+	const int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
 class swapper
 {
 public:
@@ -9,11 +16,16 @@ public:
 class simple_swapper : public swapper
 {
 public:
-	void swap(int* const a, int* const b) const override
-	{
-		const int temp = *a;
-		*a = *b;
-		*b = temp;
-	}
+	void swap(int* const a, int* const b) const override { __swap(a, b); }
 };
-class counting_swapper : public swapper{};
+
+class counting_swapper : public swapper
+{
+public:
+	counting_swapper(size_t* const counter): counter(counter) {}
+
+	void swap(int* const a, int* const b) const override { (*counter)++; __swap(a, b); }
+
+private:
+	size_t* const counter;
+};
