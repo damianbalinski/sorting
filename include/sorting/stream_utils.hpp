dfa2c3bbae55a623076514d6c2da3090c0fc9ce6
damian.balinski <damian.balinski@infinite.pl>
2021-10-21 11:16:59 +0200

    add save_results into stream utils

--- include/sorting/stream_utils.hpp
+++ include/sorting/stream_utils.hpp
@@ -1,7 +1,11 @@
 #pragma once
-#include "testing_row.hpp"
+#include "testing_identifier.hpp"
+#include "testing_results.hpp"
+#include "testing_invariants.hpp"
 #include <iostream>
 
-void save_row(std::ostream& ostream, const testing_row& row);
+void save_row(std::ostream& ostream, const testing_identifier& identifier, const testing_results& results, const testing_invariants& invariants);
 
-testing_row read_row(std::istream& istream, const testing_invariants& invariants);
+void save_results(std::ostream& ostream, const testing_results& results, const testing_invariants& invariants);
+
+testing_results read_results(std::istream& istream, const testing_identifier& identifier);
--- src/utils/stream_utils.cpp
+++ src/utils/stream_utils.cpp
@@ -5,30 +5,36 @@
 #include <fstream>
 #include <scn/scn.h>
 
-void save_row(std::ostream& ostream, const testing_row& row)
+void save_row(std::ostream& ostream, const testing_identifier& identifier,
+const testing_results& results, const testing_invariants& invariants)
 {
-	const testing_results& r = row.results;
-	const testing_invariants& i = row.invariants;
 	fmt::print(ostream, "{},{},{},{},{},{},{}\n",
-		i.id_global, i.id, r.comparisons, r.swaps, r.assigns, i.n, i.sorting_name);
+		identifier.id_global, identifier.id,
+		results.comparisons, results.swaps, results.assigns,
+		invariants.n, invariants.sorting_name);
 }
 
-testing_row read_row(std::istream& istream, const testing_invariants& invariants)
+void save_results(std::ostream& ostream, const testing_results& results,
+const testing_invariants& invariants)
+{
+	fmt::print(ostream, "{},{},{},{},{}\n",
+		invariants.sorting_name, invariants.n,
+		results.comparisons, results.swaps, results.assigns);
+}
+
+testing_results read_results(std::istream& istream, const testing_identifier& identifier)
 {
 	size_t comparisons, swaps, assigns;
-	size_t id_global, id, n;
-	std::string sorting_name;
+	size_t id_global, id;
 
 	std::string s;
 	istream >> s;
 
 	scn::scan(s, "{},{},{},{},{},{},{}",
-		id_global, id, comparisons, swaps, assigns, n, sorting_name);
+		id_global, id, comparisons, swaps, assigns);
 
-	check_num_equal(id_global, invariants.id_global);
-	check_num_equal(id, invariants.id);
-	check_num_equal(n, invariants.n);
+	check_num_equal(id_global, identifier.id_global);
+	check_num_equal(id, identifier.id);
 
-	const testing_results results{ comparisons, swaps, assigns };
-	return testing_row{ invariants, results };
+	return testing_results{ comparisons, swaps, assigns };
 }
