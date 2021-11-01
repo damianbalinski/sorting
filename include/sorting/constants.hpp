f1b1ae1639a56e5481efe0e9b847dd93548ab0f6
damian.balinski <damian.balinski@infinite.pl>
2021-11-01 21:38:55 +0100

    add program constants

--- include/sorting/constants.hpp
+++ include/sorting/constants.hpp
@@ -1,4 +1,5 @@
 #pragma once
 #include <cstdlib>
 
-#define TEST_DIRECTORY std::getenv("TEST_DIRECTORY")
+#define TEST_DIRECTORY		std::getenv("TEST_DIRECTORY")
+#define CONFIG_DIRECTORY	std::getenv("CONFIG_DIRECTORY")
