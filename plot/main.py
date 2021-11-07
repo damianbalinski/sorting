8644fba47a0a1c78ff07b6475343820be3ff4c8e
damian.balinski <damian.balinski@infinite.pl>
2021-11-07 12:04:33 +0100

    fix ploblem with encoding

--- plot/main.py
+++ plot/main.py
@@ -4,6 +4,6 @@ from path_config import *
 
 path = ConfigPath().filename('config.json')
 
-with open(path.get_path()) as config_file:
+with open(path.get_path(), encoding='utf-8') as config_file:
     data = json.load(config_file)
     Engine(data).draw()
