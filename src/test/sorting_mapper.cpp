8bc5708bbfcb81bfb6c1f31a7eeda94d4abc734d
damian.balinski <damian.balinski@infinite.pl>
2021-11-20 20:03:14 +0100

    add plot and configuration file for randomized quick-merge sort family

--- src/test/sorting_mapper.cpp
+++ src/test/sorting_mapper.cpp
@@ -50,8 +50,8 @@ const sorting* sorting_mapper::allocate(const std::string& str)
 	if (str == "dual_pivot_quick_sort") return new dual_pivot_quick_sort{new dual_pivot_quick_partition{new simple_dual_pivot_selector{}}};
 	if (str == "heap_sort")			return new heap_sort{};
 	if (str == "merge_sort")		return new merge_sort{};
-	if (str == "quick_sort")		return new quick_sort{new lemuto_partition{new simple_pivot_selector{}}};
 	if (str == "shell_sort")		return new shell_sort{};
+	if (str == "quick_sort")		return new quick_sort{ new lemuto_partition{new simple_pivot_selector{}} };
 	if (str == "intro_sort")		return new intro_sort{new lemuto_partition{new median_of_three_pivot_selector{}}};
 	if (str == "quick_merge_sort")	return new quick_merge_sort{new lemuto_partition{new simple_pivot_selector{}}};
 
@@ -72,6 +72,11 @@ const sorting* sorting_mapper::allocate(const std::string& str)
 	if (str == "quick_sort_hoare_partition_random_pivot_selector") return new quick_sort{ new hoare_partition{new random_pivot_selector{}}};
 	if (str == "quick_sort_hoare_partition_median_of_three_choices_pivot_selector") return new quick_sort{ new hoare_partition{new median_of_three_choices_pivot_selector{}}};
 	if (str == "quick_sort_hoare_partition_pseudo_median_of_nine_choices_pivot_selector") return new quick_sort{ new hoare_partition{new pseudo_median_of_nine_choices_pivot_selector{}}};
+	if (str == "quick_merge_sort_median_of_three_pivot_selector") return new quick_merge_sort{ new lemuto_partition{ new median_of_three_pivot_selector{}}};
+	if (str == "quick_merge_sort_pseudo_median_of_nine_pivot_selector") return new quick_merge_sort{ new lemuto_partition{ new pseudo_median_of_nine_pivot_selector{}}};
+	if (str == "quick_merge_sort_median_of_medians_of_three_pivot_selector") return new quick_merge_sort{ new lemuto_partition{ new median_of_medians_of_three_pivot_selector{}}};
+	if (str == "quick_merge_sort_median_of_medians_of_five_pivot_selector") return new quick_merge_sort{ new lemuto_partition{ new median_of_medians_of_five_pivot_selector{}}};
+	if (str == "quick_merge_sort_hoare_partition") return new quick_merge_sort{ new hoare_partition{ new simple_pivot_selector{}}};
 
 	fail("Unrecognized sorting algorithm");
 }
